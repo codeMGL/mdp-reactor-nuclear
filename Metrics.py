93894bb246c9ce1b52ad6e4fb0cdf6c22992bc40
Angel <alphamiller0718@gmail.com>
2026-05-15 17:11:42 +0200

    Update Metrics.py

--- Metrics.py
+++ Metrics.py
@@ -3,32 +3,20 @@ import numpy as np
 
 def MAE(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
     """ Implementation of the Mean Absolute Error (MAE) """
-    ### TO BE COMPLETED BY THE STUDENTS ###
-    
-    ### DUMMY BEHAVIOUR TO PREVENT CRASHING (MUST BE DELETED AFTER THE FULL IMPLEMENTATION) ###
-    return 0.0
-    ### ###
+    return np.float64((np.sum(abs(y_pred - y_true)))/len(y_pred))
+    # return 0.0 DUMMY
 
 def MSE(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
     """ Implementation of the Mean Squared Error (MSE) """
-    ### TO BE COMPLETED BY THE STUDENTS ###
-    
-    ### DUMMY BEHAVIOUR TO PREVENT CRASHING (MUST BE DELETED AFTER THE FULL IMPLEMENTATION) ###
-    return 0.0
-    ### ###
+    return np.float64(np.sum(abs((y_pred - y_true)**2))/len(y_pred))
+    # return 0.0 DUMMY
 
 def R2(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
     """ Implementation of the R2 metric """
-    ### TO BE COMPLETED BY THE STUDENTS ###
-    
-    ### DUMMY BEHAVIOUR TO PREVENT CRASHING (MUST BE DELETED AFTER THE FULL IMPLEMENTATION) ###
-    return 0.0
-    ### ###
+    return np.float64(1-(np.sum((y_true - y_pred)**2)/np.sum((y_true - np.mean(y_true))**2)))
+    # return 0.0 DUMMY
 
 def Corr(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
     """ Implementation of the Pearson's Correlation Coefficient """
-    ### TO BE COMPLETED BY THE STUDENTS ###
-    
-    ### DUMMY BEHAVIOUR TO PREVENT CRASHING (MUST BE DELETED AFTER THE FULL IMPLEMENTATION) ###
-    return 0.0
-    ### ###
+    return np.float64(np.sqrt((np.sum((y_true - np.mean(y_true))*(y_pred - np.mean(y_pred)))/len(y_true))/((np.sum((y_true - np.mean(y_true))**2))/len(y_true)* (np.sum((y_pred - np.mean(y_pred))**2)/len(y_pred)))))
+    # return 0.0 DUMMY
